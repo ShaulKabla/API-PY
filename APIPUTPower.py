ed4ccdb445058905ea89b3e2d6bf551dda59a200
WarCrew <130608654+ShaulKabla@users.noreply.github.com>
2024-05-07 14:20:21 +0300

    Update APIPUTPower.py

--- APIPUTPower.py
+++ APIPUTPower.py
@@ -7,8 +7,8 @@ base_url = "https://console.kamatera.com"
 test_server_id = "564d0f9c-b51e-a483-5463-cd528c32d226"
 
 # Auth keys - *Problem* - Will secure it.
-client = "19c46c2899b11d00fe359f94a826bfc8"
-secret = "d467c344850d233e7872f10561cd9505"
+client = ""
+secret = ""
 
 def put_power(): # Warning, Valid Request
     url = f"{base_url}/service/server/{test_server_id}/power"
