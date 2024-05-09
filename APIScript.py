df77a57ddc47c82259997fb527a85b6278a58d51
WarCrew <130608654+ShaulKabla@users.noreply.github.com>
2024-05-09 21:31:56 +0300

    Update APIScript.py

--- APIScript.py
+++ APIScript.py
@@ -8,9 +8,7 @@ import json
 from auth import authentication_token
 
 class WarCrewTool:
-   def __init__(self, base_url, authentication_token):
-       self.base_url = base_url
-
+   
    @staticmethod
    def display_menu():
     print("----Welcome to WarCrew Automated Tool-----")
@@ -25,14 +23,15 @@ class WarCrewTool:
 
    @staticmethod
    def list_locations():
-       list_of_locations = [
-           "IL:0"
-           "IL:0"
-           "IL:0"
-       ]
+    list_of_locations = [
+        "IL:0",
+        "IL:0",
+        "IL:0"
+    ]
 
-       print("\nList of Locations:")
+    print(f"\nList of Locations: {list_of_locations}")
 
+# Checked and varifed.
 class GetRequests:
     def __init__(self, base_url, headers, authentication_token):
         self.base_url = base_url
@@ -44,8 +43,8 @@ class GetRequests:
         if response.status_code == 200:
             ok_data = response.json()
             return ok_data
-        elif response.status_code == 201:
-            ok_data = response.json()
+        elif response.status_code == 500:  # For Debug
+            print("Error 500:")
             print(response.content.decode("utf-8"))
         else:
             print("Error:", response.status_code)
@@ -54,132 +53,142 @@ class GetRequests:
     def get_servers(self):
         url = self.base_url + "/service/servers"
         servers = self.get(url)
+        return servers
 
-        # Formating the response into clear text
+    def print_servers(self, servers):
         if servers:
-            print("\nList of Servers:")
+            print("List of Servers:")
             for server in servers:
                 print(f"Server ID: {server['id']}, Name: {server['name']}, DC: {server['datacenter']}, Power: {server['power']}")
-                print(f"\n----------------------------------------")
+                print(f"----------------------------------------")
+        else:
+            print("No servers found.")
+
+    def get_server_id_by_name(self):
+        server_name = input("Enter Server Name: ")
+        servers = self.get_servers()
+        matching_servers = [server for server in servers if server['name'] == server_name]
+        if matching_servers:
+            print("----------------------------------------")
+            for server in matching_servers:
+                print(f"Server ID for '{server_name}': {server['id']}")
+                print("----------------------------------------")
+        else:
+            print(f"No server found with the name '{server_name}'")
+
+
+
 
 class PostRequest:
-   def __init__(self, base_url, auth_token):
+   def __init__(self, base_url, headers, auth_token):
        self.base_url = base_url
        self.auth_token = auth_token
+       self.headers = headers
 
-   def put_request(self):
-       # Code for PUT request
-       pass
+   def post(self, url, data):
+        response = requests.post(url, headers=self.headers, data=data)
+        if response.status_code == 200:
+            ok_data = response.json()
+            return ok_data
+        elif response.status_code == 500: # For Debug
+            print("Error 500:")
+            print(response.content.decode("utf-8"))
+        else:
+            print("Error:", response.status_code)
+        return response
 
    def post_request(self):
-       list_of_option = [
-           "1. Create Server",
-           "2. Power on/off",
-           "3. Clone Server(Req: Source Server ID): ",
-           "4. Create New Disk For Server(Req ID): ",
-           "5. Create SnapShot For Server: ",
-           "6. Clone Disk To HDlib: "
-       ]
+        while True:
+            print('''
+                1. Create Server
+                2. Power on/off 
+                3. Clone Server(Req: Server ID): 
+                4. Create New Disk For Server(Req: ID): 
+                5. Create SnapShot For Server: 
+                6. Clone Disk To HDlib 
+            ''')
 
-       user_choice = input("Choose Action: ")
+            user_choice = input("Choose Action: ")
+            if user_choice == "1":
+                # Code for creating server
+                pass
+            elif user_choice == "2":
+                data = self.server_power()
+                # Code for power on/off
+                pass
+            elif user_choice == "3":
+                # Code for cloning server
+                pass
+            elif user_choice == "4":
+                # Code for creating new disk for server
+                pass
+            elif user_choice == "5":
+                # Code for creating snapshot for server
+                pass
+            elif user_choice == "6":
+                # Code for cloning disk to HDlib
+                pass
+            else:
+                print("Invalid choice. Please select a valid option.")
 
-       if user_choice == "1":
-           # Code for creating server
-           pass
-       elif user_choice == "2":
-           data = self.server_power()
-           # Code for power on/off
-           pass
+## Taking input from the user for creating a server
+## 
 
-       response = requests.post(url, headers=headers, data=data)
+   def user_data_server(self):
+    name_server = input("Enter the name of the server: ")
+    disk_src = input("Enter Disk Source: ") # Still not sure how the storage orginzed
+    cpu = input("Enter CPU: ")
+    ram = input("Enter Ram: ")
+    password = input("Enter Password: ")
+    billing = input("Billing (monthly/hourly): ")
+    network_name = input("Network name(wan/la-1-**lan-network-name**): ") # wan for automated process
+    
+    if network_name == "wan": # wan auto conf (can't blank - api)
+        network_ip = "192.168.100.1"
+    else:
+        network_ip = input("Enter valid IP: ")
 
-   def server_power(self):  # Post Call Func for user choice
-       power = input("1.Power On/2.Power off: ")
-       if power == "1":
-           data = {
-               "power": "on"
-           }
-       elif power == "2":
-           data = {
-               "power": "off"
-           }
-       return data
+    network_bits = "24"
+    disk_size = input("Enter Disk Size: ")
+    traffic = input("Choose Traffic(1.World/2.Asia): ")
+    if traffic == "1":
+        traffic = "t5000"
+    else:
+        traffic = "t1000"
 
-   def get_params_create_server(self):
-       name_server = input("Enter the name of the server: ")
-       cpu = input("Enter CPU: ")
-       ram = input("Enter Ram: ")
-       password = input("Enter Password: ")
-       billing = input("Billing (monthly/hourly): ")
-       network_name = "wan"
-       network_ip = "192.168.100.1"
-       network_bits = "24"
-       disk_size = input("Enter Disk Size: ")
-       traffic = input("Choose Traffic(1.World/2.Asia): ")
-       if traffic == "1":
-           traffic = "t5000"
-       else:
-           traffic = "t1000"
-       disk_src = input("Enter Disk Source: ")
+    while True:
+        datacenter = input("Enter DataCenter (type 'help' for list): ")
+        if datacenter == "help":
+            WarCrewTool.list_locations()
+        else:
+            break
 
-       while True:
-           datacenter = input("Enter DataCenter (type 'help' for list): ")
-           if datacenter == "help":
-               WarCrewTool.list_locations()
-           else:
-               break
-       if datacenter == "help":
-           return None
-       else:
-           params = {
-               "name": name_server,
-               "cpu": cpu,
-               "ram": ram,
-               "password": password,
-               "billing": billing,
-               "network_name_0": network_name,
-               "network_ip_0": network_ip,
-               "network_bits_0": network_bits,
-               "disk_size_0": disk_size,
-               "traffic": traffic,
-               "disk_src_0": disk_src,
-               "datacenter": datacenter
-           }
+    if datacenter == "help":
+        return None
+    else:
+        params = {
+            "name": name_server,
+            "cpu": cpu,
+            "ram": ram,
+            "password": password,
+            "billing": billing,
+            "network_name_0": network_name,
+            "network_ip_0": network_ip,
+            "network_bits_0": network_bits,
+            "disk_size_0": disk_size,
+            "traffic": traffic,
+            "disk_src_0": disk_src,
+            "datacenter": datacenter
+        }
 
-           return params
+        return params
 
-   def create_servers(self):
+
+   def create_server(self):
        url = self.base_url + "/service/server"
 
-       headers = {
-           "AuthClientId": client,
-           "AuthSecret": secret,
-           "Content-Type": "application/x-www-form-urlencoded"
-       }
 
-       num_servers = int(input("Enter the number of servers to create: "))
-
-       valid_params = self.get_params_create_server()
-
-       if not valid_params:
-           print("Invalid parameters. Exiting Script.")
-           return
-
-       for _ in range(num_servers):
-           response = requests.post(url, headers=headers, data=valid_params)
-           if response.status_code == 200:
-               ok_data = response.json()
-               print(f"Data for 200 OK Response: {ok_data}")
-           elif response.status_code == 201:
-               ok_data = response.json()
-               print(f"Data for 201 Created Response: {ok_data}")
-           elif response.status_code == 500:
-               print("Error 500 Internal Server Error. Response content:")
-               print(response.content.decode("utf-8"))
-           else:
-               print("Error:", response.status_code)
-
-       print("All servers created successfully.")
+       response = requests.post(url, headers=headers, data=data)
 
 def main():
     base_url = "https://console.kamatera.com"
@@ -192,15 +201,15 @@ def main():
         choice = WarCrewTool.display_menu()
 
         if choice == "1":
-            PostRequest(base_url,headers, authentication_token).create_servers()
+            PostRequest(base_url, headers, authentication_token).create_servers()
         elif choice == "2":
             WarCrewTool.list_locations()
         elif choice == "3":
-            # Code for managing client
-            pass
+            GetRequests(base_url, headers, authentication_token).get_server_id_by_name()
         elif choice == "4":
             get_requests = GetRequests(base_url, headers, authentication_token)
-            get_requests.get_servers()
+            servers = get_requests.get_servers()
+            get_requests.print_servers(servers)
         elif choice == "5":
             print("----Thank You for using WarCrew Automated Tool-----")
             break
@@ -208,4 +217,4 @@ def main():
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
-   main()
+    main()
